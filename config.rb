cd69bf04be265ba113aa46a916abc9f4f12bacee
Roger <dzz3825390@gmail.com>
2020-03-31 15:48:36 +0800

    fix order_detail_cash page

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
-# Set this to the root of your project when deployed:
+
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
@@ -14,7 +14,6 @@ javascripts_dir = "javascripts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
 
